11c001f0417e0333ed45e3d3db45e3c5b6ca641b
Sam Hiner <25464398+samhiner@users.noreply.github.com>
2018-12-04 11:03:34 -0500

    Destroy SESSION

--- src/index.php
+++ src/index.php
@@ -3,10 +3,15 @@
 
 	//TODO fix me later
 
-	if (!(isset($_SESSION['asdf']))) {
-		$_SESSION['userData'] = '';
-		$_SESSION['asdf'] = 'yay';
-	}
+	//remove PHPSESSID from browser
+if ( isset( $_COOKIE[session_name()] ) )
+setcookie( session_name(), “”, time()-3600, “/” );
+//clear session from globals
+$_SESSION = array();
+//clear session from disk
+session_destroy();
+
+
 	
 	//END TODO
 
